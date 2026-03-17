7b03c27590f3145c363191ad029b986d335e643b
AncheJeez <andresjsangon@gmail.com>
2026-03-17 21:45:02 +0100

    arreglado un bug y modivo symbols.sql fuera de carpetas

--- back-end/src/main/java/com/marketsimulator/back_end/security/SecurityConfig.java
+++ back-end/src/main/java/com/marketsimulator/back_end/security/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -20,7 +19,7 @@ public class SecurityConfig {
 	@Order(1)
 	public SecurityFilterChain authSecurityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.securityMatcher(new AntPathRequestMatcher("/api/auth/**"))
+			.securityMatcher("/api/auth/**")
 			.csrf(csrf -> csrf.disable())
 			.cors(Customizer.withDefaults())
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
